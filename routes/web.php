ee1e57778914b20fd13ef7c62221d00a5d1f0959
Adinda Kurnia Rifanti <adindakkr@gmail.com>
2023-02-22 16:25:31 +0700

    Praktikum 2

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+//use App\Http\Controllers\PageController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticlesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,14 +17,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    echo "Selamat Datang";
-});
+// Route::get('/', function () {
+//     echo "Selamat Datang";
+// });
 
-Route::get('/about', function () {
-    echo "2141720100-Adinda Kurnia Rifanti";
-});
+// Route::get('/about', function () {
+//     echo "2141720100-Adinda Kurnia Rifanti";
+// });
 
-Route::get('/articles/{id}', function ($id) {
-    echo "Halaman Artikel dengan ID {id}";
-});
+// Route::get('/articles/{id}', function ($id) {
+//     echo "Halaman Artikel dengan ID {id}";
+// });
+
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/about', [AboutController::class, 'about']);
+Route::get('/articles/{id}', [ArticlesController::class, 'articles']);
